c7f7e5b22ed2f041e6eae5543a9cee5bf5bc98e2
Mateusz Lipczynski <mlipcz@wp.pl>
2019-11-02 10:40:07 +0100

    Set up initial dependencies

---
+++ src/main/java/pl/modulo/kidsmathquiz/KidsmathquizApplication.java
@@ -0,0 +1,13 @@
+package pl.modulo.kidsmathquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsmathquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsmathquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/modulo/kidsmathquiz/KidsmathquizApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.modulo.kidsmathquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsmathquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
